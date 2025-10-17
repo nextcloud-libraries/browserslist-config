5c75ae7c5998dc7d084466c6d200cbcafb85285d
skjnldsv <skjnldsv@protonmail.com>
2025-10-17 10:10:19 +0200

    chore: test the config to ensure it is valid
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

---
+++ __tests__/browserlist.test.mjs
@@ -0,0 +1,16 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+import browserslistConfig from '../browserlist.config.js'
+import browserslist from 'browserslist'
+import { getUserAgentRegex } from 'browserslist-useragent-regexp'
+
+// Generate a regex that matches user agents to detect incompatible browsers
+const supportedBrowsersRegExp = getUserAgentRegex({ allowHigherVersions: true, browsers: browserslistConfig })
+const supportedBrowsers = browserslist(browserslistConfig)
+
+// Log the results
+console.log('Supported browsers regex for Nextcloud Web:', supportedBrowsersRegExp)
+console.log('Supported browsers for Nextcloud Web:', supportedBrowsers.join(', '))
+
