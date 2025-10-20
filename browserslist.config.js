b6f188062ffbe1ab3b69c15b17a11f5dcf4f6e61
Grigorii K. Shartsev <me@shgk.me>
2025-10-20 12:51:34 +0200

    chore: manually lint
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- browserslist.config.js
+++ browserslist.config.js
@@ -1,11 +1,12 @@
-/**
+/*
  * SPDX-FileCopyrightText: 2019 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
+
 module.exports = [
-  'defaults',
-  'not op_mini all',
-  'not dead',
-  'Firefox ESR',
-  'baseline widely available',
-];
+	'defaults',
+	'not op_mini all',
+	'not dead',
+	'Firefox ESR',
+	'baseline widely available',
+]
